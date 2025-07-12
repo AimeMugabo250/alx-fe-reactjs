d8169f29772371450cef95d97eb584b0d5f3b9de
mugabo aime <mugaboaime888@gmail.com>
2025-07-12 15:43:03 +0200

    Add WelcomeMessage component and integrate into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,5 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,8 +8,9 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
+    <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,6 +24,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -30,6 +34,7 @@ function App() {
       </p>
     </>
   )
+  
 }
 
 export default App
