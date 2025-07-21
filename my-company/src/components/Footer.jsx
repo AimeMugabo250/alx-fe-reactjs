cceae8997f5f1669842ed03ddaefc8c22d5456b9
Aime Mugabo <mugaboaime222@gmail.com>
2025-07-21 13:27:52 +0200

    Add files via upload

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ textAlign: 'center', marginTop: '40px', padding: '10px', backgroundColor: '#eee' }}>
+        <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
