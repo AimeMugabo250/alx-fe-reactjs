22521df21e453e4a676a4a644bcbe31a89ef2302
mugabo aime <mugaboaime888@gmail.com>
2025-07-22 14:38:12 +0200

    .

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -15,3 +15,18 @@ function App() {
 }
 
 export default App;
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import RecipeList from './components/RecipeList';
+
+function App() {
+  return (
+    <div style={{ maxWidth: '600px', margin: 'auto', padding: '1rem' }}>
+      <h1>Recipe Sharing App</h1>
+      <SearchBar />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
