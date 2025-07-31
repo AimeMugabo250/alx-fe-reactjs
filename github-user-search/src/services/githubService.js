e2c9decf91fc8144511acc216cd6169e2c4d6310
Aime Mugabo <mugaboaime222@gmail.com>
2025-07-31 16:02:52 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -13,3 +13,18 @@ export const fetchUserData = async (username) => {
     throw new Error('Failed to fetch user data');
   }
 };
+
+export const fetchUsersByAdvancedSearch = async ({ username, location, minRepos }) => {
+  let query = '';
+
+  if (username) query += `${username} in:login`;
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
+  const response = await fetch(`https://api.github.com/search/users?q=${encodeURIComponent(query)}&per_page=10`);
+  const data = await response.json();
+
+  if (!response.ok) throw new Error(data.message || "Search failed");
+
+  return data.items;
+};
