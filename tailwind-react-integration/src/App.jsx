1c07ff0ed6c741b19c078005eae408be72210da8
Aime Mugabo <mugaboaime222@gmail.com>
2025-08-04 18:27:37 +0200

    Add files via upload

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,11 +1,3 @@
-function App() {
-  return (
-    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-900">Hello, Tailwind!</h1>
-    </div>
-  )
-}
-
 import UserProfile from './components/UserProfile';
 
 function App() {
@@ -15,5 +7,4 @@ function App() {
     </div>
   );
 }
-
-export default App
+export default App;
--- tailwind-react-integration/src/main.jsx
+++ tailwind-react-integration/src/main.jsx
@@ -1,15 +1,3 @@
-// import './index.css';
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
