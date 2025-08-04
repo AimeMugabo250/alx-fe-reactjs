b9f4421783ce5e627284ef7d06ec812b9f25e331
Aime Mugabo <mugaboaime222@gmail.com>
2025-08-04 17:35:50 +0200

    Add files via upload

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-600 text-center mt-10">
-      Tailwind CSS is working!
-    </h1>
-  );
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-900">Hello, Tailwind!</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
