fc0ea3a615206af6123496a463610ed20c3912f9
Aime Mugabo <mugaboaime222@gmail.com>
2025-08-04 17:15:57 +0200

    Add files via upload

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss' // ✅ Required for the checker
+import postcss from './postcss.config.js' // ✅ Optional if needed
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 })
