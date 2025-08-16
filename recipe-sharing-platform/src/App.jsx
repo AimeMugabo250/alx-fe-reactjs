eda59926c23184a2677ac5c622cbbcc16b8684ad
Aime Mugabo <mugaboaime222@gmail.com>
2025-08-16 15:17:50 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -3,7 +3,6 @@ function App() {
     <h1 className="text-3xl font-bold text-blue-500">
       Recipe Sharing Platform
     </h1>
-    <h1 className="text-4xl font-bold text-red-500">Hello Tailwind</h1>
   )
 }
 
