fd2aff45269ef1b1416559122a57789f2491a839
Aime Mugabo <mugaboaime222@gmail.com>
2025-08-16 15:43:01 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -6,4 +6,14 @@ function App() {
   )
 }
 
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <HomePage />
+    </div>
+  );
+}
+
 export default App;
