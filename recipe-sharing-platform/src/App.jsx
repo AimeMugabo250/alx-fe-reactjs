70544694ecaef2bf790dd51ef8fc58dbe4b0a79c
Aime Mugabo <mugaboaime222@gmail.com>
2025-08-16 15:56:46 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -16,4 +16,24 @@ function App() {
   );
 }
 
+
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
 export default App;
+
