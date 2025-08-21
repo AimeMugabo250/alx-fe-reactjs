2f81902dbbb1da54e475c254f75442c5af17839e
Aime Mugabo <mugaboaime222@gmail.com>
2025-08-21 18:20:26 +0200

    Update TodoList.test.js

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -13,23 +13,16 @@ describe("TodoList Component", () => {
     render(<TodoList />);
     const input = screen.getByLabelText("new-todo-input");
     const button = screen.getByText("Add");
-
     fireEvent.change(input, { target: { value: "Write tests" } });
     fireEvent.click(button);
-
     expect(screen.getByText("Write tests")).toBeInTheDocument();
   });
 
-  test("toggles a todo item", () => {
+  test("toggles a todo", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
-
-    // initial: not completed
     expect(todo).not.toHaveStyle("text-decoration: line-through");
-
     fireEvent.click(todo);
-
-    // after toggle
     expect(todo).toHaveStyle("text-decoration: line-through");
   });
 
@@ -37,9 +30,7 @@ describe("TodoList Component", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
     const deleteButton = todo.querySelector("button");
-
     fireEvent.click(deleteButton);
-
     expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
   });
 });
