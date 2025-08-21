9034b64b809ad4e3097b620476b6cc52cb3b15d2
Aime Mugabo <mugaboaime222@gmail.com>
2025-08-21 18:06:44 +0200

    Add files via upload

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,45 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByLabelText("new-todo-input");
+    const button = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "Write tests" } });
+    fireEvent.click(button);
+
+    expect(screen.getByText("Write tests")).toBeInTheDocument();
+  });
+
+  test("toggles a todo item", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+
+    // initial: not completed
+    expect(todo).not.toHaveStyle("text-decoration: line-through");
+
+    fireEvent.click(todo);
+
+    // after toggle
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    const deleteButton = todo.querySelector("button");
+
+    fireEvent.click(deleteButton);
+
+    expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+  });
+});
